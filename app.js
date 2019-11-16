9b3bd23c6e9b96f9ed8790fff9978bcc41f2a59c
Jackie <jackie.ko95@gmail.com>
2019-11-16 15:55:00 -0500

    Added js and hamburger icon

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('#nav-icon3').click(function(){
+    $(this).toggleClass('open').toggleClass('close');
+    $(".overlay").fadeToggle(200);
+  });
+});
+
+$('.close').on('click', function() {
+  $(".overlay").fadeToggle(200);
+  $('#nav-icon3').toggleClass('open').toggleClass('close');
+  open = false;
+})
+
+$('#overlay-btn').on('click', function() {
+  $(".overlay").fadeToggle(200);
+  $('#nav-icon3').toggleClass('open').toggleClass('close');
+  open = false;
+})
